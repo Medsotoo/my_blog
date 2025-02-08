d50cbb2db48062fba6e71603d0ca46c15b51206e
Сергей Пальчиков <s_palchikov@example.com>
2025-02-08 11:56:19 +0300

    начал процесс создания вьюхи(создал класс MainView), вытаскиваем из бд все посты и возврощаем их для отображения в файл index.html, осталось в index.html пройтись циклом по всем постам и отобразить в надлежащем виде, пока отобразил весь список постов(в классе post отображается только title в методе __str__)

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
 from django.views import View
 
+from .models import Post
+
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        return render(
-            request,
-            'myblog/index.html'
-        )
+        posts = Post.objects.all()
+        return render(request, 'myblog/index.html', context={
+            'posts': posts
+        })
