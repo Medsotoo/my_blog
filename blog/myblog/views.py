c79aaf54a25be89e21760c90b35e508f1b9156f8
Сергей Пальчиков <s_palchikov@example.com>
2025-02-09 17:34:35 +0300

    обновил вьюху, добавит туда пагинацию, изменил index.html изменил блок пагинации, добавил состояния кнопок пагинации и проверки на наличие блоков

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,5 +1,6 @@
 
 # Create your views here.
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import View
 
@@ -8,7 +9,12 @@ from .models import Post
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
-        posts = Post.objects.all()
+        posts = Post.objects.all().order_by('-created_at')
+        paginator = Paginator(posts, 6)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
         return render(request, 'myblog/index.html', context={
-            'posts': posts
+            'page_obj': page_obj
         })
