6f95fd17b355371098fc1b90224e45c812972aa9
Сергей Пальчиков <s_palchikov@example.com>
2025-02-13 17:51:06 +0300

    создал форму регистрации form.py прописал поля с помощью виджетов, добавиль вьюху регистрации, и путь в urls.py, создал шаблон signup.html закрепил кнопку регистрации в блоке navbar

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,11 +1,12 @@
 
 # Create your views here.
-from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.views import View
-
+from django.core.paginator import Paginator
 from .models import Post
-
+from .forms import SignUpForm
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
@@ -26,3 +27,23 @@ class PostDetailView(View):
         return render(request, 'myblog/post_detail.html', context={
             'post': post
     })
+
+
+
+class SignUpView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignUpForm()
+        return render(request, 'myblog/signup.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+        return render(request, 'myblog/signup.html', context={
+            'form': form,
+        })
