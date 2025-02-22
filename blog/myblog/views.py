04600e88fd8c655fec29c9b0fd00a5848cc34d7b
Сергей Пальчиков <s_palchikov@example.com>
2025-02-22 15:28:05 +0300

    добавил авторизацию (форму,вьюху,урл,страницу html)

--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator
 from .models import Post
-from .forms import SignUpForm
-from django.contrib.auth import login
+from .forms import SignUpForm,SignInForm
+from django.contrib.auth import login, authenticate
 from django.http import HttpResponseRedirect
 
 class MainView(View):
@@ -45,5 +45,29 @@ class SignUpView(View):
                 login(request, user)
                 return HttpResponseRedirect('/')
         return render(request, 'myblog/signup.html', context={
+            'form': form,
+        })
+    
+
+class SignInView(View):
+    def get(self, request, *args, **kwargs):
+        form = SignInForm()
+        return render(request, 'myblog/signin.html', context={
+            'form': form,
+        })
+
+    def post(self, request, *args, **kwargs):
+        form = SignInForm(request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                form.add_error(None, "Неправильный пароль или указанная учётная запись не существует!")
+                return render(request, "myblog/signin.html", {"form": form})
+        return render(request, 'myblog/signin.html', context={
             'form': form,
         })
